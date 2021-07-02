638538af1a661884b4574bbc5333ef8fa9445170
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-07-02 12:11:15 +0200

    Evènement de formulaire  et Datatransformer

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -84,6 +84,8 @@ class ProductController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
+            dd($form->getData());
+
             $product->setSlug(strtolower($slugger->slug($product->getName())));
             $em->flush();
 
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,13 +4,18 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Form\DataTransformer\CentimesTransformer;
+use App\Form\Type\PriceType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -24,11 +29,12 @@ class ProductType extends AbstractType
             ])
             ->add("shortDescription", TextareaType::class, [
                 "label" => "Courte Description",
-                "attr" => ["placeholder" => "Tapez une description courte mais parlante pour le visiteur"]
+                "attr" => ["placeholder" => "Tapez une description courte mais parlante pour le visiteur"],
             ])
             ->add("price", MoneyType::class, [
                 "label" => "Prix du produit",
-                "attr" => ["placeholder" => "Tapez le prix du produit en €"]
+                "attr" => ["placeholder" => "Tapez le prix du produit en €"],
+                "divisor" => 100
             ])
             ->add("mainPicture", UrlType::class, [
                 "label" => "Image du produit",
@@ -38,10 +44,45 @@ class ProductType extends AbstractType
                 "label" => "Catégorie",
                 "placeholder" => "-- Choisir une catégorie --",
                 "class" => Category::class,
-                "choice_label" => function (Category $category) { // Cette fonction permet d'ecrire les nom de catégorie en majuscule et d'aller chercher les catégories dans la base de données
+                "choice_label" => function (Category $category) { // Cette fonction permet d'ecrire les noms de catégorie en majuscule et d'aller chercher les catégories dans la base de données
                     return strtoupper($category->getName());
                 }
             ]);
+
+        // $builder->get("price")->addModelTransformer(new CentimesTransformer);
+
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });
+
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+
+        //     $form = $event->getForm();
+
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
+
+
+        //     // if ($product->getId() === null) {
+        //     //     $form->add("category", EntityType::class, [
+        //     //         "label" => "Catégorie",
+        //     //         "placeholder" => "-- Choisir une catégorie --",
+        //     //         "class" => Category::class,
+        //     //         "choice_label" => function (Category $category) { // Cette fonction permet d'ecrire les nom de catégorie en majuscule et d'aller chercher les catégories dans la base de données
+        //     //             return strtoupper($category->getName());
+        //     //         }
+        //     //     ]);
+        //     // }
+        // });
     }
 
     public function configureOptions(OptionsResolver $resolver)
