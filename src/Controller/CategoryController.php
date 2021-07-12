f211fd233f28d0874aef1aed869cc722d0d48f81
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-07-12 11:22:15 +0200

    Authentification ok securité ok

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,11 +6,15 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends AbstractController
 {
@@ -33,9 +37,26 @@ class CategoryController extends AbstractController
 
     /**
      * @Route("/admin/category/create", name="category_create")
+     * 
      */
-    public function categoryCreate(Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    public function categoryCreate(Request $request, SluggerInterface $slugger, EntityManagerInterface $em, Security $security)
     {
+
+        // $this->denyAccessUnlessGranted("ROLE_ADMIN", null, "Vous n'avez pas accès à cette ressource");
+        // $user = $security->getUser(); // Permet de recuperer l'utilisateur actuel
+        // $user = $this->getUser();
+
+        // if ($user === null) {
+        //     return $this->redirectToRoute("security_login");
+        // }
+
+
+        // if ($this->isGranted("ROLE_ADMIN") === false) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le droit d'accèder à cette ressource");
+        // }
+
+
+
         $category = new Category;
         $form = $this->createForm(CategoryType::class, $category);
 
@@ -60,10 +81,17 @@ class CategoryController extends AbstractController
     /**
      *
      * @Route("/admin/category/{id}/edit", name="category_edit")
+     * 
      */
-    public function categoryEdit($id, CategoryRepository $categoryRepository, Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    public function categoryEdit($id, CategoryRepository $categoryRepository, Request $request, SluggerInterface $slugger, EntityManagerInterface $em, Security $security)
     {
+        // $this->denyAccessUnlessGranted("ROLE_ADMIN", null, "Vous n'avez pas accès à cette ressource");
+
         $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            throw new NotFoundHttpException("Cette catégorie n'existe pas !");
+        }
 
         $form = $this->createForm(CategoryType::class, $category);
 
