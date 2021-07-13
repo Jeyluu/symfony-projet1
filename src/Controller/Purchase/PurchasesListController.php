7cca895d6e434a9f87dbb2f757033c522607d31b
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-07-13 18:48:08 +0200

    Liaison des produits et commandes

--- src/Controller/Purchase/PurchasesListController.php
+++ src/Controller/Purchase/PurchasesListController.php
@@ -2,32 +2,24 @@
 
 namespace App\Controller\Purchase;
 
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Entity\User;
+use Twig\Environment;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Security;
-use Twig\Environment;
 
 class PurchasesListController extends AbstractController
 {
-    protected $security;
-    protected $router;
-    protected $twig;
-
-    public function __construct(Security $security, RouterInterface $router, Environment $twig)
-    {
-        $this->security = $security;
-        $this->router = $router;
-        $this->twig = $twig;
-    }
-
 
     /**
      *
      * @Route("/purchases", name="purchase_index")
+     * @IsGranted("ROLE_USER", message="Vous etes être connecté pour accèder à vos commandes")
      */
     public function index()
     {
@@ -35,20 +27,14 @@ class PurchasesListController extends AbstractController
         /**
          * @var User
          */
-        $user = $this->security->getUser();
+        $user = $this->getUser();
 
-        if (!$user) {
-            // Redirection -> RedirectResponse
-            // Générer une URL en fonction du nom d'une route ->urlGenerator ou RouterInterface
-            throw new AccessDeniedException("Vous devez être connecté pour accès à vos commandes.");
-        }
+
         // 2.Nous voulons savoir QUI est connecté ->security
 
         // 3.Nous voulons passer l'utilisateur connecté à Twig afin d'afficher ses commandes.
-        $html = $this->twig->render("purchase/index.html.twig", [
+        return $this->render("purchase/index.html.twig", [
             "purchases" => $user->getPurchases()
         ]);
-
-        return new Response($html);
     }
 }
