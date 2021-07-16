47f2c124ed6f88529122de2e8324b355fe273146
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-07-16 12:02:03 +0200

    fin module de paiement stripe

---
+++ src/Stripe/StripeService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Stripe;
+
+use App\Entity\Purchase;
+
+class StripeService 
+{
+
+    protected $secretKey;
+    protected $publicKey;
+
+    public function __construct(string $secretKey, string $publicKey )
+    {
+        $this->secretKey = $secretKey;
+        $this->publicKey = $publicKey;
+    }
+    public function getPublicKey(): string
+    {
+        return $this->publicKey;
+    }
+
+    public function getPaymentIntent(Purchase $purchase){
+        // This is your real test secret API key.
+        \Stripe\Stripe::setApiKey($this->secretKey);
+
+        return \Stripe\PaymentIntent::create([
+            "amount" => $purchase->getTotal(),
+            "currency" => "eur"
+        ]);
+    }
+}
